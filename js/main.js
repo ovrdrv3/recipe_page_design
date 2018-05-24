fb458fda02459ccdf7d99f42d0474e5a5e0a78fb
Donaven Snowden <Donaven@cipdirect.com>
2018-05-24 14:03:41 -0700

    update JSON data

--- js/main.js
+++ js/main.js
@@ -1,56 +1,61 @@
-var ingredients = {
-    1: {
-        amount: "1/2",
-        amountDesc: "cup",
-        food: "bittersweet chocolate chips"
-    },
-    2 : {
-        amount: "6",
-        amountDesc: "tablespoons",
-        food: "heavy whipping cream, divided"
-    },
-    
-    3: {
-        amount: "3/4",
-        amountDesc: "cup",
-        food: "mascarpone cheese"
-    },
-    
-    4: {
-        amount: "3",
-        amountDesc: "tablespoons",
-        food: "amaretto"
-    },
-    
-    5: {
-        amount: "8 1/2",
-        amountDesc: "inch-thick",
-        food: "angel food cake slices"
-    },
-    
-    6: {
-        amount: "2",
-        amountDesc: "tablespoons",
-        food: "sugar"
-    },
-    
-    7: {
-        amount: "24 1/3",
-        amountDesc: "inch-thick",
-        food : "diagonal banana slices (from about 3 bananas)"
-    },
-    
-    8: {
-        amount: "1/3",
-        amountDesc: "cup",
-        food: "sliced almonds, toasted"
-    }    
-    
-};
+// http://jsonapi.org/format/
+var recipeData = 
 
-function getFood(obj){
+{
+ "ingredients" : [
+    {
+        sort: "1",
+        amount: "1/2 cup",
+        ingredient: "bittersweet chocolate chips"
+    },
+    {
+        sort: "2",
+        amount: "6 tablespoons",
+        ingredient: "heavy whipping cream, divided"
+    },
+    
+    {
+        sort: "3",
+        amount: "3/4 cup",
+        ingredient: "mascarpone cheese"
+    },
+    
+    {
+        sort: "4",
+        amount: "3 tablespoons",
+        ingredient: "amaretto"
+    },
+    
+    {
+        sort: "5",
+        amount: "8 1/2 inch-thick",
+        ingredient: "angel food cake slices"
+    },
+    
+    {
+        sort: "6",
+        amount: "2 tablespoons",
+        ingredient: "sugar"
+    },
+    
+    {
+        sort: "7",
+        amount: "24",
+        ingredient : "diagonal banana slices (from about 3 bananas)"
+    },
+    
+    {
+        sort: "8",
+        amount: "1/3 cup",
+        ingredient: "sliced almonds, toasted"
+    }    
+]};
+    
+
+
+function getingredient(obj){
     for (let index = 0; index < obj.length; index++) {
         
-        console.log(obj[index].food);        
+        console.log(obj[index].ingredient);        
     }
 }
